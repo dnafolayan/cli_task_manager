450fb6d73286cc3c5b0d062736a07e2e2ed3ceea
Divine <divineafolayan05@gmail.com>
2025-06-25 19:47:11 +0100

    add functionality for adding and viewing tasks

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ func validateChoice(choice int) (bool, error) {
 }
 
 func main() {
-	// tasks := []Task{}
+	nextID := 1
+	tasks := []Task{}
 
 	fmt.Println("Welcome to the Task Manager!\n\nPlease choose an option:\n1. Add Task\n2. View Tasks\n3. Mark Task as Completed\n4. Exit")
 
@@ -29,6 +30,28 @@ func main() {
 		} else if _, err := validateChoice(choice); err != nil {
 			fmt.Println(err)
 			continue
+		}
+
+		switch choice {
+		case 1:
+			var description string
+			fmt.Print("Enter task description: ")
+			fmt.Scan(&description)
+
+			task := Task{nextID, description, false}
+			tasks = append(tasks, task)
+			fmt.Printf("Task added: %v\n", task)
+			nextID++
+		case 2:
+			if len(tasks) == 0 {
+				fmt.Println("No tasks available.")
+			} else {
+				fmt.Println("Tasks:")
+				for _, task := range tasks {
+					fmt.Printf("ID: %d, Description: %s, Completed: %t\n", task.ID, task.Description, task.Completed)
+				}
+			}
+
 		}
 	}
 }
